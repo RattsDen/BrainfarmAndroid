6dc9f1464c6b9b2267d8ad8011c975a4e93aa956
Eric Thompson <anyxius@gmail.com>
2017-09-22 18:38:43 -0400

    Cleaned up register activity

--- app/src/main/java/ca/brainfarm/RegisterActivity.java
+++ app/src/main/java/ca/brainfarm/RegisterActivity.java
@@ -2,8 +2,11 @@ package ca.brainfarm;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import ca.brainfarm.data.User;
 import ca.brainfarm.serviceclient.FaultHandler;
 import ca.brainfarm.serviceclient.ServiceCall;
 import ca.brainfarm.serviceclient.ServiceFaultException;
@@ -15,6 +18,7 @@ public class RegisterActivity extends BaseBrainfarmActivity {
     private EditText txtPassword;
     private EditText txtPasswordConfirm;
     private EditText txtEmail;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +29,24 @@ public class RegisterActivity extends BaseBrainfarmActivity {
         txtPassword = (EditText)findViewById(R.id.txtPassword);
         txtPasswordConfirm = (EditText)findViewById(R.id.txtPasswordConfirm);
         txtEmail = (EditText)findViewById(R.id.txtEmail);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
     }
 
     public void btnRegisterClicked(View view) {
         // TODO: check that password fields match
-        registerUser();
+        if (passwordFieldsMatch()) {
+            // Make service call
+            registerUser();
+        } else {
+            String warning = getString(R.string.warn_password_match);
+            Toast.makeText(this, warning, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean passwordFieldsMatch() {
+        String p1 = txtPassword.getText().toString();
+        String p2 = txtPasswordConfirm.getText().toString();
+        return p1.equals(p2);
     }
 
     private void registerUser() {
@@ -41,15 +58,17 @@ public class RegisterActivity extends BaseBrainfarmActivity {
         registerUser.addArgument("username", username);
         registerUser.addArgument("password", password);
         registerUser.addArgument("email", email);
-        registerUser.execute(Boolean.class, new SuccessHandler<Boolean>() {
+        registerUser.execute(User.class, new SuccessHandler<User>() {
             @Override
-            public void handleSuccess(Boolean result) {
-
+            public void handleSuccess(User result) {
+                String message = "Registration Successful!";
+                Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
             }
         }, new FaultHandler() {
             @Override
             public void handleFault(ServiceFaultException ex) {
-
+                // Show toast with exception message
+                Toast.makeText(RegisterActivity.this, ex.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
